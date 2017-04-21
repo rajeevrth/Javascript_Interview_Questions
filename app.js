0264af9ea2f9edd0677e8aca01ac1c55f8102983
rajeevrth <rajeevkumarsharma@outlook.com>
2017-04-21 13:19:38 +0530

    check in

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//1. What will the code below output to the console and why?
+//1. What will be the output and why?
 var a = b = 3;
 
 console.dir(a);
@@ -6,35 +6,35 @@ console.dir(b);
 console.log("a defined? " + (typeof a !== 'undefined'));
 console.log("b defined? " + (typeof b !== 'undefined'));
 
-//2. What will the code below output to the console and why?
-var obj = {
-    name: 'Rajeev',
-    myName: function () {
-        var my = this;
-        console.log("outer func:  this.foo = " + this.name);
-        console.log("outer func:  this.foo = " + my.name);
+//2. Palindrome using Recursion
+var a = 0, b = 1;
+var number = 0;
+function Palindrome(a, b) {
+    while (number < 8) {
+        console.log(a + b);
+        number++;
+        Palindrome(b, a + b);
     }
 }
-obj.myName();
+console.log(a);
+console.log(b);
+Palindrome(a, b);
 
-//3. What will the code below output to the console and why?
-function foo1() {
-    return {
-        bar: "hello"
-    };
-}
-
-function foo2() {
-    return 
+var str="Rajeev";
+var reversedString='';
+var index=str.length-1;
+function Reverse(str,index)
+{
+    if(index<0)
     {
-        bar: "hello"
-    };
+        console.log(reversedString);
+    }
+    else
+    {
+        reversedString=reversedString+str[index];
+        Reverse(str,index-1);
+    }
 }
+Reverse(str,index);
 
-console.log("foo1 returns:");
-console.log(foo2()); //=> undefined
-/*Returns Undefined because , when the line containing the return statement (with nothing else on the line) is encountered in foo2(), 
-a semicolon is automatically inserted immediately after the return statement.*/
-console.log("foo2 returns:");
-console.log(foo1()); //=> Object {bar: "hello"}
-                        // bar:"hello"
+
